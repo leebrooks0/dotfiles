801e7aa1764cdd6284df2c80e02a949a846c9ad9
Lee Brooks <leebrooks0@gmail.com>
2015-02-17 21:25:44 +0200

    added oh my fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
+# Hide "(venv)prompt>" as fish theme includes venv info
+set VIRTUAL_ENV_DISABLE_PROMPT 1
+
 # oh-my-fish
 set fish_path $HOME/.oh-my-fish
 set fish_theme bobthefish
-set fish_plugins extract python z
+set fish_plugins extract python
 . $fish_path/oh-my-fish.fish
 
 # Use 256 colours
@@ -12,5 +15,14 @@ set -x PYENV_ROOT "$HOME/.pyenv"
 set -x PATH "$PYENV_ROOT/bin" $PATH
 status --is-interactive; and . (pyenv init -|psub)
 
-# Use Vim by default
 set EDITOR vim
+
+function fish_greeting
+end
+
+function fish_right_prompt
+  set_color $fish_color_autosuggestion[1]
+  set __python_version (python -V | cut -c1-)
+  set __ruby_version Ruby (ruby -e "print RUBY_VERSION")
+  echo "$__python_version; $__ruby_version"
+end
