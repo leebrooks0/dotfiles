5111561059dcd0311638a550ccec86723775932c
Lee Brooks <leebrooks0@gmail.com>
2015-06-03 17:28:50 +0200

    Use NPM

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,9 @@ set -x PYENV_ROOT "$HOME/.pyenv"
 set -x PATH "$PYENV_ROOT/bin" $PATH
 status --is-interactive; and . (pyenv init -|psub)
 
+# nvm
+nvm use stable > /dev/null
+
 # Use Neovim everywhere
 set -x EDITOR nvim
 
