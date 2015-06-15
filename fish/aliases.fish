cdf3e8455bf51fd8b3f593d8165962f679d1df85
Lee Brooks <lee@kagiso.io>
2015-06-15 12:56:37 +0200

    Added some shell aliases

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,1 +1,15 @@
+# General
+alias f fg
+alias ll "ls -Ahl"
+alias py python
 alias v nvim
+
+# Django
+alias dd "python manage.py dbshell"
+alias dmm "python manage.py makemigrations"
+alias dm "python manage.py migrate"
+alias dr "python manage.py runserver"
+alias ds "python manage.py shell"
+alias dt "python manage.py test"
+
+# Git
